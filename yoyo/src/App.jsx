954a51688f13fdcc4f51a424cb39162d2cab9a17
Pavanpatil326 <143817491+Pavanpatil326@users.noreply.github.com>
2024-12-10 13:30:29 +0530

    pheli commit

---
+++ yoyo/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+let [counter, setCounter] = useState(15)
+
+
+
+
+   
+  //  let counter = 5
+ 
+   const addValue = () => {
+    if(counter===20) return;
+     setCounter(counter + 1)
+     console.log("clicked", counter);
+   }
+
+   const removeValue = () => {
+    if(counter===0) return;
+     setCounter(counter - 1)
+     console.log("clicked", counter);
+   }
+
+  return (
+    <>
+       <h1>YOYO Hooks </h1>
+       <h2>Counter Value: {counter}</h2>    
+
+
+       <button onClick={addValue}>Add Value{counter}</button>
+       <br />
+       <button onClick={removeValue}>Remove Value {counter}</button>
+       <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
